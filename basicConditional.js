f259f6b381f03191e1948b290ac2f6b7bdf8ae4f
Vincent Sadino Guizot <vincentguizot128@gmail.com>
2021-01-14 14:30:51 +0700

    basic conditional

---
+++ basicConditional.js
@@ -0,0 +1,94 @@
+//Variable
+
+//Tipe Data Primitive
+/**
+ * 1. String
+ * 2. Numbers
+ * 3. Boolean
+ * 4. Object
+ * 5. Undefined
+ * 6. Null
+ */
+//Deklarasi Variable
+// var angka;
+
+//Inisialisasi Variable -> Memasukan value ke dalam variable
+// var angka = "false";
+// console.log(angka);
+
+// if(angka){
+//     console.log("Ini True");
+// }else {
+//     console.log("Ini False")
+// }
+
+//Switch - case
+
+/**
+ * SOAL
+ * jika level Senior -> 
+ *      jika exp lebih dari 5 tahun -> gaji pokok ditambah 5000000
+ *      jika exp kurang dari 5 tahun -> gaji pokok ditambah 3000000
+ * jika level Middle -> 
+ *      jika exp lebih dari 5 tahun -> gaji pokok ditambah 1500000
+ *      jika exp kurang dari 5 tahun -> gaji pokok ditambah 500000
+ * jika level Junior -> tidak ada tambahan 
+ * */
+
+// var level = "etc";
+// var experience = 3;
+// var mainSalary = 5000000;
+
+// if(nama === "Vincent"){
+//     console.log("Nama kamu adalah " + nama);
+// }else if(nama === "Mario"){
+//     console.log("Nama kamu adalah " + nama);
+// }else {
+//     console.log("Nama lain")
+// }
+// var totalSalary;
+
+// switch(level){
+//     case "Senior":
+//         if(experience > 5){
+//             totalSalary = mainSalary + 5000000;
+//         }else if(experience < 5){
+//             totalSalary = mainSalary + 3000000;
+//         }
+//         console.log("Level kamu adalah " + level + ", dan gaji kamu adalah " + totalSalary + ", Experience kamu adalah : " + experience + " tahun");
+//     break;
+//     case "Middle":
+//         if(experience > 5){
+//             totalSalary = mainSalary + 1500000;
+//         }else if(experience < 5){
+//             totalSalary = mainSalary + 500000;
+//         }
+//         console.log("Level kamu adalah " + level + ", dan gaji kamu adalah " + totalSalary + ", Experience kamu adalah : " + experience + " tahun");
+//     break;
+//     case "Junior":
+//         totalSalary = mainSalary
+//         console.log("Level kamu adalah " + level + ", dan gaji kamu adalah " + totalSalary + ", Experience kamu adalah : " + experience + " tahun");
+//     break;
+//     default:
+//         console.log("Tidak ada level tersebut");
+//     break;
+        
+// }
+
+//Nested If
+// if(x < 10) {
+//     if(x === 5){
+
+//     }
+// }
+
+// var number = 0;
+// console.log(number)
+
+//Jenis angka
+/**
+ * 1. Decimal : 0,1,2,...,9
+ * 2. Biner : 0,1
+ * 3. Octal : 0,1,..,7
+ * 4. Hexadecimal : 0,1,2,...,9,A,B,C,D,E,F
+ */
